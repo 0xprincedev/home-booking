e142bfeb5a8836c618aacf7d4c77b199a13df08d
risto0301 <risto0301@gmail.com>
2022-07-21 02:02:22 -0400

    update scss

--- src/views/Home/Section/MainCarousel.tsx
+++ src/views/Home/Section/MainCarousel.tsx
@@ -5,32 +5,25 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'
 const carousalImageList = [
 	{ imageUrl: '/assets/images/carousel/1.jpg', title: 'Legend' },
 	{ imageUrl: '/assets/images/carousel/2.jpg', title: 'Legend' },
-  { imageUrl: '/assets/images/carousel/3.jpg', title: 'Legend' },
-  { imageUrl: '/assets/images/carousel/4.jpg', title: 'Legend' },
-  { imageUrl: '/assets/images/carousel/5.jpg', title: 'Legend' },
-  { imageUrl: '/assets/images/carousel/6.jpg', title: 'Legend' },
+	{ imageUrl: '/assets/images/carousel/3.jpg', title: 'Legend' },
+	{ imageUrl: '/assets/images/carousel/4.jpg', title: 'Legend' },
+	{ imageUrl: '/assets/images/carousel/5.jpg', title: 'Legend' },
+	{ imageUrl: '/assets/images/carousel/6.jpg', title: 'Legend' },
 ]
 
 const MainCarousel = () => {
-	const handleChange = () => {}
-	const handleClickItem = () => {}
-	const handleClickThumb = () => {}
-
 	return (
 		<div>
-			<Carousel
-				showArrows={true}
-				onChange={handleChange}
-				onClickItem={handleClickItem}
-				onClickThumb={handleClickThumb}
-			>
-				{carousalImageList.map((carousalItem, index) => (
-					<div>
-						<img src={carousalItem.imageUrl} alt='carousal' />
-						<p className='legend'>{carousalItem.title}</p>
-					</div>
-				))}
-			</Carousel>
+			<div className='container'>
+				<Carousel>
+					{carousalImageList.map((carousalItem, index) => (
+						<div>
+							<img src={carousalItem.imageUrl} alt='carousal' />
+							{/* <p className='legend'>{carousalItem.title}</p> */}
+						</div>
+					))}
+				</Carousel>
+			</div>
 		</div>
 	)
 }
